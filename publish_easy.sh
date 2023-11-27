922e64529e0b5ce57c1977266ac286a343edda58
Fran Simó <fransimo@gmail.com>
2023-11-27 12:42:37 +0100

    v1.3

--- publish_easy.sh
+++ publish_easy.sh
@@ -7,12 +7,15 @@ project_server="sanghasanya.com"
 
 dir=`pwd`
 
+source=${dir}/public/
+destination=${project_user}@${project_server}:/home/${project_user}/${project_server}
+
+# rm -fR $source/*
 docker run --rm -it \
   -v $(pwd):/src \
   klakegg/hugo:0.101.0-ext
 
-source=${dir}/public/
-destination=${project_user}@${project_server}:/home/${project_user}/${project_server}
+
 
 # Newer files first. Drafts becoming published and new JPGs show go first /index.html or any archive to avoid 404
 rsync -avh --ignore-existing  $source  $destination >> $HOME/nohup.out
